7eeeff136950564812c7b597e26656779e2d87d3
Uche-chukwu-m <alihuchechukwumichael@gmail.com>
2024-05-16 00:51:03 -0500

    made little tweaks to code added the iamges folder

--- chessEngine.py
+++ chessEngine.py
@@ -1,10 +1,10 @@
-#where we store all the information about the current state of a chess game. also responsible for determinint the valid moves of the current state. 
+#where we store all the information about the current state of a chess game. also responsible for determining the valid moves of the current state. 
 #it will also keep a move log
 
 class GameState():
     def __init__(self):
         #board is 8x8 two dimensional list and each element has 2 characters
-        #the firsst character represents the color of the piece, "b" or "w"
+        #the first character represents the color of the piece, "b" or "w"
         #the second character represents the type of the piece, "K","Q","R","B","N",or "p"
         #the string "--" represents an empty space with no piece
         self.board = [
@@ -20,7 +20,7 @@ class GameState():
         self.whiteToMove = True
         self.moveLog = []
     
-    def makeMove(self,move):
+    def makeMove(self, move):
         self.board[move.startRow][move.startCol] = "--"
         self.board[move.endRow][move.endCol] = move.pieceMoved
         self.moveLog.append(move) #log the move so we can undo it later
@@ -47,7 +47,7 @@ class Move():
 
 
     def getChessNotation(self):
-        #make this real chess notation
+        #make this real chess notation later
         return self.getRankFile(self.startRow,self.startCol) +self.getRankFile(self.endRow,self.endCol)
                
 
